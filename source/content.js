1ef08f0a6835fa068f614125db655e7b71c65c3d
Luke Camilleri <mail@camilleriluke.com>
2018-05-08 08:01:30 +0200

    Emebed viewers on pages that are loaded via AJAX

--- source/content.js
+++ source/content.js
@@ -3,11 +3,21 @@ import domLoaded from 'dom-loaded';
 import safeElementReady from './libs/safe-element-ready';
 import { selectFigmaLinks, embedViewerAfterTag } from './embed-figma-viewer';
 
+const embedViewers = () => selectFigmaLinks(document).map(embedViewerAfterTag);
+
 async function init() {
 	await safeElementReady('body');
-
 	await domLoaded;
-	selectFigmaLinks(document).map(embedViewerAfterTag);
+
+	// Let's emebed any viewers that are visible on the page at loading time
+	embedViewers();
+
+	// Now we need to listen for an event that is fired every time a pages is
+	// changed via AJAX (this is highly coupled to GitHub's internal
+	// implementation) and embed viewers again.
+	document.addEventListener('pjax:end', () => {
+		embedViewers();
+	});
 }
 
 init();
